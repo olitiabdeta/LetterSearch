e79310fa13248afb5eedada74f070ac157b88a93
Oliti Abdeta <olab6122@colorado.edu>
2025-11-18 19:58:10 -0700

    Initial commit, build with springboot

---
+++ src/main/java/com/finalproject/lettersearch/LetterSearchApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.lettersearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterSearchApplication.class, args);
+    }
+
+}
