cb44690727b846a072157def4d029be83298d543
Oliti Abdeta <olab6122@colorado.edu>
2025-12-02 10:33:51 -0700

    added pagecontroller and changes to gamecontroller

--- src/main/java/com/finalproject/lettersearch/controller/GameController.java
+++ src/main/java/com/finalproject/lettersearch/controller/GameController.java
@@ -1,14 +1,60 @@
 package com.finalproject.lettersearch.controller;
 
+import com.finalproject.lettersearch.data.Word;
+import com.finalproject.lettersearch.factory.SearchFactory;
+import com.finalproject.lettersearch.service.Game;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 
 public class GameController {
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    private Game game;
+    private final Map<String, SearchFactory> factoryMap = new HashMap<>();
+
+    @GetMapping("/start")
+      public GameDTO start(String theme){
+        SearchFactory factory = factoryMap.get(theme);
+        if (factory==null){
+            //error message
+            throw new IllegalArgumentException("Theme " +theme+" not found");
+        }
+        game = new Game(factory);
+        game.startGame();
+        var board = game.getBoard();
+        char[][] boardGrid = board.getBoard();
+        List<Word> words = board.getWords();
+        return new GameDTO( boardGrid,words);
+
     }
+
+     public static class GameDTO{
+        private final char[][] board;
+        private final List<Word> words;
+        public GameDTO(char[][] board, List<Word> words) {
+            this.board = board;
+            this.words = words;
+        }
+        public char[][] getBoard() {
+            return board;
+        }
+        public List<Word> getWords() {
+            return words;
+        }
+     }
+
+
+
+
+
+
+
+
 
 }
