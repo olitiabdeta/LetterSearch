7b81691fdc134c471c9f6b14521596504c2667e4
Oliti Abdeta <olab6122@colorado.edu>
2025-12-04 17:09:13 -0700

    Changes to observer to carry a list of found words and update score by 5 points

--- src/main/java/com/finalproject/lettersearch/observer/ListObserver.java
+++ src/main/java/com/finalproject/lettersearch/observer/ListObserver.java
@@ -4,12 +4,21 @@ import com.finalproject.lettersearch.data.Word;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListObserver implements IObserver {
     private static final Logger logger = LoggerFactory.getLogger(ListObserver.class);
-
+    private final List<String> foundWords = new ArrayList<>();
     @Override
     public void update(Word word) {
-        logger.info("List updated, found word: {}", word.getWord());
+        if (!foundWords.contains(word.getWord())) {
+            foundWords.add(word.getWord());
+        }
+        logger.info("List updated, found words: {}", foundWords);
         //will fill in once gamestatemanager is complete
+    }
+    public List<String> getFoundWords() {
+        return foundWords;
     }
 }
--- src/main/java/com/finalproject/lettersearch/observer/ScoreObserver.java
+++ src/main/java/com/finalproject/lettersearch/observer/ScoreObserver.java
@@ -6,10 +6,18 @@ import org.slf4j.LoggerFactory;
 
 public class ScoreObserver implements IObserver {
     private static final Logger logger = LoggerFactory.getLogger(ScoreObserver.class);
+    private int score = 0;
+    private static final int WORD_POINTS = 5;
     @Override
     public void update(Word word) {
+        if (word.isFound()) {
+            score+=WORD_POINTS;
+            logger.info("Score updated, found word: {}, score: {}", word.getWord(),  score);
+        }
         //will fill in once gamestatemanager is complete
-        logger.info("Score updated, found word: {}", word.getWord());
 
+    }
+    public int getScore() {
+        return score;
     }
 }
