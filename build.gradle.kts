f40c8848a73543bf43a013ad717ffb0a45bc8b44
Oliti Abdeta <olab6122@colorado.edu>
2026-03-30 16:47:36 -0600

    Update Java toolchain to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ description = "LetterSearch"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
